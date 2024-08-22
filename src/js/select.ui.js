e211640153c94d03741e098cb77425ada5dd50a0
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-08-22 17:23:38 +1200

    [Refactor] ui: lifecycle

--- src/js/select.ui.js
+++ src/js/select.ui.js
@@ -56,12 +56,12 @@ class UISelection extends Selection {
 
 		// Data
 		this.data = undefined;
+		this.key = undefined;
 		this.dataType = Type.Null;
 		this.rendered = new Map();
-		this.effector = this.renderer.bind(this);
 		this.condition = undefined;
 		// Interaction/Behavior
-		this.behavior = {};
+		this.behavior = undefined;
 		// TODO: Initial state parsing
 	}
 
@@ -72,11 +72,11 @@ class UISelection extends Selection {
 	}
 
 	apply(data) {
-		return AppliedUISelection(this, data);
+		return new AppliedUISelection(this, data);
 	}
 
 	does(behavior) {
-		Object.assign(this.behavior, behavior);
+		this.behavior = Object.assign(this.behavior ?? {}, behavior);
 		return this;
 	}
 
@@ -84,11 +84,13 @@ class UISelection extends Selection {
 	// DATA
 	// ========================================================================
 
-	set(data) {
+	set(data, key = this.key) {
+		this.key = key;
 		this.render(data);
 	}
 
-	update(data) {
+	update(data, key = this.key) {
+		this.key = key;
 		this.render(data);
 	}
 
@@ -97,89 +99,86 @@ class UISelection extends Selection {
 	// ========================================================================
 
 	// --
-	// Creates, updates or removes a selection based on the arguments.
-	renderer(current, key, state = undefined, previous = undefined) {
-		if (state === undefined) {
-			// Create a new instance of the selection
-			const ui = this.clone();
-			// We bind the event handlers
-			for (const k in this.on) {
-				const handlers = this.behavior[k];
-				if (handlers) {
-					for (const event in handlers) {
-						const h = handlers[event];
-						ui.on[k].bind(event, (event) => {
-							return h.apply(ui.data ?? current, [
-								event,
-								ui,
-								key,
-							]);
-						});
-					}
-				}
-			}
-			// We update it
-			console.log("CREATE", current, this.behavior);
-			ui.renderer(current, key, ui);
-			return ui;
-		} else if (current === undefined) {
-			// Remove
-			console.log("REMOVE", key, previous);
-			return this.remove();
-		} else {
-			// Update
-			console.log(
-				"UPDATE",
-				this,
-				":",
-				[previous, current],
-				key,
-				this.out,
-			);
-			// Taking care of behaviour. Note that the behaviour
-			// will apply to all the states.
-			console.log("UPDATE", current, this.behavior);
-			for (const k in this.behavior) {
-				const ui = this.out[k];
-				if (ui) {
-					const v = this.behavior[k];
-					console.log("BEHAVIOR", k, "=", v);
-					if (v instanceof UISelection) {
-						// Behaviour is a selection, so we map the
-						// current value to it.
-						ui.render(current, v);
-					} else if (v instanceof Function) {
-						// Otherwise it's a function, we either…
-						const w = v.apply(current, [key, ui]);
-						// TODO: We may want to have an applied selection as well
-						if (w instanceof UISelection) {
-							// … produces a UISelection (dynamic component)
-							ui.render(current, w);
-						} else if (w instanceof AppliedUISelection) {
-							ui.render(w.data, w.selection);
-						} else {
-							// … or a derived value that we then render
-							ui.render(w);
-						}
-					} else {
-						ui.render(v);
-					}
-				}
-			}
-			// Taking care of when (showing and hiding)
-			for (const { predicate, node, placeholder } of this.when) {
-				const v = predicate.apply(current || {}, ui);
-				if (v) {
-					if (!node.parentNode) {
-						placeholder.parentNode.replaceChild(node, placeholder);
-					}
-				} else {
-					if (!placeholder.parentNode) {
-						node.parentNode.replaceChild(placeholder, node);
-					}
+	// Creates, updates or removes a selection based on the argument
+	doCreate(data, key = this.key) {
+		// Create a new instance of the selection
+		const ui = this.clone();
+		// We bind the event handlers
+		for (const k in this.on) {
+			const handlers = this.behavior[k];
+			if (handlers) {
+				for (const event in handlers) {
+					const h = handlers[event];
+					ui.on[k].bind(event, (event) => {
+						return h.apply(ui.data ?? data, [event, ui, key]);
+					});
 				}
 			}
 		}
+		ui.attr("data-xxx", "POUET");
+		ui.set(data, key);
+		return ui;
+	}
+
+	doRemove() {
+		this.remove();
+		this.data = undefined;
+		this.key = undefined;
+		return this;
+	}
+
+	doUpdate(data = this.data, key = this.key) {
+		// Update
+		// Taking care of behaviour. Note that the behaviour
+		// will apply to all the states.
+		for (const k in this.behavior) {
+			const ui = this.out[k];
+			if (ui) {
+				const v = this.behavior[k];
+				if (v instanceof UISelection) {
+					// Behaviour is a selection, so we map the
+					// current value to it.
+					ui.render(data, v);
+				} else if (v instanceof Function) {
+					// Otherwise it's a function, we either…
+					const w = v.apply(data, [data, key, ui]);
+					// TODO: We may want to have an applied selection as well
+					if (w instanceof UISelection) {
+						// … produces a UISelection (dynamic component)
+						ui.render(data, w);
+					} else if (w instanceof AppliedUISelection) {
+						ui.render(w.data, w.selection);
+					} else {
+						// … or a derived value that we then render
+						ui.render(w);
+					}
+				} else {
+					ui.render(v);
+				}
+			}
+		}
+		// Taking care of when (showing and hiding)
+		for (const { predicate, node, placeholder } of this.when) {
+			const v = predicate.apply(data || {}, ui);
+			if (v) {
+				if (!node.parentNode) {
+					placeholder.parentNode.replaceChild(node, placeholder);
+				}
+			} else {
+				if (!placeholder.parentNode) {
+					node.parentNode.replaceChild(placeholder, node);
+				}
+			}
+		}
+		this.data = data;
+		this.key = key;
+	}
+
+	doClear() {
+		for (const v of this.rendered.values()) {
+			v.doRemove();
+		}
+		this.rendered.clear();
 	}
 
 	// ========================================================================
@@ -189,91 +188,85 @@ class UISelection extends Selection {
 	// --
 	// Renders the given data, using `create`, `update` and `remove`
 	// functions
-	render(data, renderer = undefined) {
-		const create =
-			renderer instanceof UISelection ? renderer.effector : renderer;
-		const update = create;
-		const remove = update;
+	render(data, ui = undefined) {
 		const data_type = type(data);
-		if (!renderer) {
-			// This is not a mapping, we render a single value
+		if (!ui) {
 			// TODO: Should detect a change
-			this.unrender();
-			this.text(data);
+			if (this.behavior) {
+				// If there's a behavior, then it means we need to do a render
+				// update of all the slots.
+				this.doUpdate(data);
+			} else {
+				// This is not a mapping, we render a single value
+				this.doClear();
+				this.text(data);
+			}
 		} else {
 			switch (data_type) {
 				case Type.Null:
-					this.unrender();
+					this.doClear();
 					break;
 				case Type.Atom:
 					{
 						// TODO: We should detect changes
-						const r = this.rendered.get(null);
-						this.unrender();
-						const _ = create(data, null, r, this.data);
-						this.rendered.set(null, (this.append(_), _));
+						let r = this.rendered.get(null);
+						if (r) {
+							r.doUpdate(data);
+						} else {
+							r = ui.doCreate(data);
+							this.append(r);
+							this.rendered.set(null, r);
+						}
 					}
 					break;
 				case Type.List:
 					if (this.dataType === data_type) {
 						const n = Math.min(this.data.length, data.length);
 						for (let i = 0; i < n; i++) {
-							update(
-								data[i],
-								i,
-								this.rendered.get(i),
-								this.data[i],
-							);
+							this.rendered.get(i).doUpdate(data[i], i);
 						}
 						for (let i = n; i < data.length; i++) {
-							const _ = create(data[i], i);
-							this.rendered.set(null, (this.append(_), _));
+							const r = ui.doCreate(data[i], i);
+							this.append(r);
+							this.rendered.set(i, r);
 						}
 						for (let i = n; i < this.data.length; i++) {
-							remove(
-								undefined,
-								i,
-								this.rendered.get(i),
-								this.data[i],
-							);
+							console.log("XXX REMOVE ITEM", i);
+							this.rendered.get(i).doRemove();
+							this.rendered.delete(i);
 						}
 					} else {
-						this.unrender();
+						this.doClear();
 						for (let i = 0; i < data.length; i++) {
-							const _ = create(data[i], i);
-							this.rendered.set(i, (this.append(_), _));
+							const r = ui.doCreate(data[i], i);
+							this.append(r);
+							this.rendered.set(i, r);
 						}
 					}
 					break;
 				case Type.Dict:
 					if (this.dataType === data_type) {
 						for (const k in data) {
-							if (this.data[i] === undefined) {
-								const _ = create(data[k], k);
-								this.rendered.set(k, (this.append(_), _));
+							if (this.data[k] === undefined) {
+								const r = ui.doCreate(data[k], k);
+								this.append(r);
+								this.rendered.set(k, r);
 							} else {
-								update(
-									data[k],
-									k,
-									this.rendered.get(k),
-									this.data[k],
-								);
+								this.rendered.get(k).update(data[k], k);
 							}
 						}
 						for (const k in this.data) {
-							if (this.data[i] === undefined) {
-								remove(
-									undefined,
-									k,
-									this.rendered.get(k),
-									this.data[k],
-								);
+							if (this.data[k] === undefined) {
+								this.rendered.get(k).doRemove();
+								this.rendered.delete(k);
 							}
 						}
 					} else {
-						this.unrender();
-						for (let i = n; i < data.length; i++) {
-							create(data[i], i);
+						this.doClear();
+						for (const k in data) {
+							const r = ui.doCreate(data[k], k);
+							this.append(r);
+							this.rendered.set(k, r);
 						}
 					}
 					break;
@@ -281,12 +274,7 @@ class UISelection extends Selection {
 		}
 		this.data = data;
 		this.dataType = data_type;
-	}
-	unrender() {
-		for (const v of this.rendered.values()) {
-			v.remove();
-		}
-		this.rendered.clear();
+		return this;
 	}
 }
 
